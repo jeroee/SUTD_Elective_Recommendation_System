270078e8589a503e23a840474d0d88c9dd1abc85
Jeremy Ng <56480078+jeroee@users.noreply.github.com>
2021-07-26 19:48:33 +0800

    update

---
+++ scripts/query.py
@@ -0,0 +1,43 @@
+import numpy as np
+import string
+from gensim.models import KeyedVectors
+from gensim.parsing.preprocessing import remove_stopwords
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+lemmatizer = WordNetLemmatizer()
+
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+
+def process_query(query):
+    query = query.translate(str.maketrans('', '', string.punctuation))
+    query = str(np.char.lower(query))
+    query = ''.join([i for i in query if not i.isdigit()])
+    query = remove_stopwords(query)
+    query = [lemmatizer.lemmatize(w, get_wordnet_pos(w))
+             for w in nltk.word_tokenize(query)]
+    query = ' '.join([str(elem) for elem in query])
+    query = query.translate(str.maketrans('', '', string.punctuation))
+    return query.split()
+
+def expand_query(query,glove_kv):
+    model = KeyedVectors.load(glove_kv)
+    expanded_query = []
+    for word in query:
+        expanded_query.append(word)
+        if word in model:
+            for close_word in model.most_similar(word, topn = 3):
+                expanded_query.append(close_word[0])
+    return expanded_query
+
